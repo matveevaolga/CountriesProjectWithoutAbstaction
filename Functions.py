fc972c39eab62153824766e87f2ed84e95ff2e99
Olga Matveeva <matveevaolga2882@gmail.com>
2023-09-25 14:05:12 +0300

    fixed functions in Functions module

--- Functions.py
+++ Functions.py
@@ -1,127 +1,110 @@
 from Classes import Army, Economy, Country, InputError
-import random
 
 
-def start_a_war(country1: Country, country2: Country) -> None:
-    if country1.army_money <= 0 or not country2.army_money <= 0:
-        print(f"Невозможно начать атаку, так как в одной или обеих из выбранных стран нет денег на армию.")
-        return
-# деньги нужны только на найм
-    print(f"Выбор атакующей армии для страны {country1.country_name}:")
-    army1 = choose_army(country1)
-    print(f"Выбор атакующей армии для страны {country2.country_name}:")
-    army2 = choose_army(country2)
-# не работает при отсутствии армий
-    if not army1 or not army2:
-        print(f"Невозможно начать атаку, так как в одной или обеих из стран нет созданных армий.")
-        return
+def update_fights_stats(active_country: Country, passive_country: Country, fight_info: dict) -> None:
+    active_army, passive_army = fight_info['active_army'], fight_info['passive_army']
+    active_result, passive_result = fight_info['active_result'], fight_info['passive_result']
 
-    if not army1.number_of_soldiers or not army2.number_of_soldiers:
-        print(f"Невозможно начать атаку, так как в одной или обеих из выбранных армий нет солдат.")
-        return
+    active_army.fights_info['fights_total'] += 1
+    if passive_army: passive_army.fights_info['fights_total'] += 1
 
-    soldiers_lost1 = random.randint(0, country1.number_of_soldiers)
-    soldiers_lost2 = random.randint(0, country2.number_of_soldiers)
-
-    result1 = (country1.number_of_soldiers + country1.possible_number_of_recruits -
-               soldiers_lost1) * army1.army_strength * (country1.army_money if country1.army_money > 0 else 1)
-    result2 = (country2.number_of_soldiers + country2.possible_number_of_recruits -
-               soldiers_lost2) * army2.army_strength * (country2.army_money if country2.army_money > 0 else 1)
-# с 31 до 43 в отдельную ф-цию
-    army1.fights_info['fights_total'] += 1
-    army2.fights_info['fights_total'] += 1
-
-    army1.number_of_soldiers -= soldiers_lost1
-    army2.number_of_soldiers -= soldiers_lost2
-
-    country1.population -= soldiers_lost1
-    country2.population -= soldiers_lost2
-
-    army1.loss_of_money += soldiers_lost1 * 5
-    army2.loss_of_money += soldiers_lost2 * 5
-    country1.army_money += soldiers_lost2 * 5 - soldiers_lost1 * 5
-    country2.army_money += soldiers_lost1 * 5 - soldiers_lost2 * 5
-# тоже в отдельную ф-цию до 54
-    if result1 > result2:
-        army1.fights_info['fights_won'] += 1
-        army2.fights_info['fights_lost'] += 1
-        winner, loser = country1, country2
-        wlost, llost = soldiers_lost1, soldiers_lost2
+    if active_result > passive_result:
+        active_army.fights_info['fights_won'] += 1
+        if passive_army: passive_army.fights_info['fights_lost'] += 1
+        winner, loser = active_country, passive_country
     else:
-        army2.fights_info['fights_won'] += 1
-        army1.fights_info['fights_lost'] += 1
-        winner, loser = country2, country1
-        wlost, llost = soldiers_lost2, soldiers_lost1
+        passive_army.fights_info['fights_won'] += 1
+        active_army.fights_info['fights_lost'] += 1
+        winner, loser = passive_country, active_country
 
-    print(f"Армия страны {winner.country_name} побеждает, потеряв {wlost} солдат.")
-    print(f"Армия страны {loser.country_name} проигрывает, потеряв {llost} солдат.")
+    print(f"Армия страны {winner.country_name} побеждает.")
+    print(f"Армия страны {loser.country_name} проигрывает.")
 
 
-def recruit_soldiers(country: Country) -> None:
+def start_a_war(active_country: Country, passive_country: Country) -> bool:
+    print(f"Выбор атакующей армии для страны {active_country.country_name}:")
+    active_army = choose_army(active_country)
+    print(f"Выбор атакующей армии для страны {passive_country.country_name}:")
+    passive_army = choose_army(passive_country)
+
+    if not active_army or not active_army.number_of_soldiers:
+        print(f"Невозможно начать атаку, так как в атакующей стране"
+              f" нет созданных армий или в выбранной армии нет солдат.")
+        return False
+
+    active_result = (active_country.number_of_soldiers * active_army.army_strength *
+                     (active_country.army_money if active_country.army_money > 0 else 1))
+    passive_result = -1 if (not passive_army or not passive_country.number_of_soldiers) else \
+        (passive_country.number_of_soldiers * passive_army.army_strength *
+         (passive_country.army_money if passive_country.army_money > 0 else 1))
+
+    fight_info = {'active_army': active_army, 'passive_army': passive_army,
+                  'active_result': active_result, 'passive_result': passive_result}
+
+    update_fights_stats(active_country, passive_country, fight_info)
+
+    return True
+
+
+def recruit_soldiers(country: Country, given_army=None) -> bool:
+    army = choose_army(country) if not given_army else given_army
+    try:
+        amount = int(input(f"Введите желаемое количество солдат для рекрутинга"
+                           f" (всего доступно {country.possible_number_of_recruits})."))
+        if amount > country.possible_number_of_recruits:
+            raise InputError
+        army.number_of_soldiers += amount
+        print(f"{amount} солдат были рекрутированы.")
+        return True
+    except ValueError:
+        print("Введено не число.")
+    except InputError:
+        print("Введенное количество солдат превышает максимально возможное для рекрутинга.")
+    return False
+
+
+def retire_soldiers(country: Country) -> bool:
     army = choose_army(country)
-    while True:
-        try:
-            amount = int(input(f"Введите желаемое количество солдат для рекрутинга (всего доступно {country.possible_number_of_recruits})."))
-            if amount > country.possible_number_of_recruits:
-                raise InputError
-            break
-        except ValueError:
-            print("Введено не число.")
-        except InputError:
-            print("Введенное количество солдат превышает максимально возможное для рекрутинга.")
-    army.number_of_soldiers += amount
-    country.army_money += amount * 5
-    country.economy_money -= amount * 5
-    print(f"{amount} солдат были рекрутированы.")
+    try:
+        amount = int(input(f"Введите желаемое количество солдат для"
+                           f" отставки (всего доступно {army.number_of_soldiers})."))
+        if amount > army.number_of_soldiers:
+            raise InputError
+        army.number_of_soldiers -= amount
+        print(f"{amount} солдат были отправлены в отставку.")
+        return True
+    except ValueError:
+        print("Введено не число.")
+    except InputError:
+        print("Введенное количество солдат превышает максимально возможное для отставки.")
+    return False
 
 
-def retire_soldiers(country: Country) -> None:
-    army = choose_army(country)
-    while True:
-        try:
-            amount = int(input(f"Введите желаемое количество солдат для отставки (всего доступно {army.number_of_soldiers})."))
-            if amount > army.number_of_soldiers:
-                raise InputError
-            break
-        except ValueError:
-            print("Введено не число.")
-        except InputError:
-            print("Введенное количество солдат превышает максимально возможное для отставки.")
-    army.number_of_soldiers -= amount
-    country.army_money -= amount * 5
-    country.economy_money += amount * 5
-    print(f"{amount} солдат были отправлены в отставку.")
-
-
-# нет денег -> другое действие, а не while
-def buy_upgrades(country: Country) -> None:
+def buy_upgrades(country: Country) -> bool:
     cost = country.economy.upgrade_cost
     maxamount = country.economy_money // cost
     if maxamount <= 0:
         print("Невозможно купить улучшения из-за недостатка денег.")
-        return
-    while True:
-        try:
-            amount = int(input(f"Введите желаемое количество улучшений (всего доступно {maxamount})."))
-            if amount > maxamount:
-                raise InputError
-            print("Улучшения куплены.")
-            break
-        except ValueError:
-            print("Введено не число.")
-        except InputError:
-            print("Введенное количество улучшений превышает максимально возможное.")
-    country.economy_money -= amount * cost
-    country.economy_money += amount * 1000
+        return False
+    try:
+        amount = int(input(f"Введите желаемое количество улучшений (всего доступно {maxamount})."))
+        if amount > maxamount:
+            raise InputError
+        print("Улучшения куплены.")
+        country.economy_money -= amount * cost
+        country.economy_money += amount * 1000
+        return True
+    except ValueError:
+        print("Введено не число.")
+    except InputError:
+        print("Введенное количество улучшений превышает максимально возможное.")
+    return False
 
 
-def create_country(taken_names: list[str]) -> (Country, str):
+def fill_economy() -> Economy:
+    rows = ["upgrade_cost", "number_of_upgrades"]
     economy = Economy()
-    country = Country()
-    for_economy = ["upgrade_cost", "number_of_upgrades"]
-    for_country = ["country_name", "population", "economy_money", "army_money", "income"]
-# в отдельную ф-цию заполнение страны и отд. для заполнения экономики
-    for row in for_economy:
+    for row in rows:
         while True:
             try:
                 info = int(input(f"Заполнение поля {row}: "))
@@ -133,7 +116,12 @@ def create_country(taken_names: list[str]) -> (Country, str):
                 print(f"Введено не число.")
             except InputError:
                 print("Данное поле не может принимать введенное значение.")
+    return economy
 
+
+def fill_country(taken_names: list[str]) -> Country:
+    country = Country()
+    for_country = ["country_name", "population", "economy_money", "army_money", "income"]
     for row in for_country:
         if row == "country_name":
             while True:
@@ -157,43 +145,43 @@ def create_country(taken_names: list[str]) -> (Country, str):
                     print(f"Введено не число.")
                 except InputError:
                     print("Данное поле не может принимать введенное значение.")
+        return country
 
-    country.economy = economy
+
+def create_country(taken_names: list[str]) -> (Country, str):
+    country = fill_country(taken_names)
+    country.economy = fill_economy()
     return country, country.country_name
 
 
-# тоже fill_army
-def create_army() -> Army:
+def fill_army(country: Country) -> Army:
     army = Army()
-    for_army = ["general", "number_of_soldiers"]
-    for row in for_army:
-        if row == "general":
-            while True:
-                try:
-                    info = input(f"Заполнение поля {row}: ")
-                    if not info.isalpha():
-                        raise InputError
-                    setattr(army, row, info)
-                    break
-                except InputError:
-                    print("Генерал не может иметь введенное имя.")
-        else:
-            while True:
-                try:
-                    info = int(input(f"Заполнение поля {row}: "))
-                    if info <= 0:
-                        raise InputError
-                    setattr(army, row, info)
-                    break
-                except ValueError:
-                    print(f"Введено не число.")
-                except InputError:
-                    print("Данное поле не может принимать введенное значение.")
+    while True:
+        try:
+            info = input(f"Заполнение поля general: ")
+            if not info.isalpha():
+                raise InputError
+            setattr(army, "general", info)
+            break
+        except InputError:
+            print("Генерал не может иметь введенное имя.")
+    print("Заполнение поля number_of_soldiers: ")
+    while not recruit_soldiers(country, army):
+        print("Заполнение поля number_of_soldiers: ")
+    return army
+
+
+def create_army(country: Country) -> Army:
+    army = fill_army(country)
     print(f"Армия успешно создана.")
     return army
 
 
-def choose_army(country: Country) -> Army:
+def choose_army(country: Country):
+    if not len(country.armies):
+        return None
+    print(f"Вот информация об армиях страны {country.country_name}:")
+    for i in range(len(country.armies)): print(f"Армия {i}:\n{country.armies[i]}")
     while True:
         try:
             army_number = int(input(f"Введите номер армии. Всего у страны {country.country_name} "
@@ -208,12 +196,41 @@ def choose_army(country: Country) -> Army:
     return country.armies[army_number - 1]
 
 
-# перенести деньги на экономику с армии + еще одно действие
-def economy_stage(active_country: Country) -> None:
-    actions = {1: "Купить улучшения", 2: "Пропустить ход"}
-    # убрать zip
-    for n, a in zip(actions.keys(), actions.values()): print(f"{n}: {a}")
-    # тоже можно убрать while
+def get_money_for_army(country: Country) -> bool:
+    try:
+        amount = int(input(f"Введите желаемое количество денег для перевода (всего доступно {country.economy_money}."))
+        if amount > country.economy_money:
+            raise InputError
+        print("Деньги переведены.")
+        country.economy_money -= amount
+        country.army_money += amount
+        return True
+    except ValueError:
+        print("Введено не число.")
+    except InputError:
+        print("Введенное количество денег превышает максимально доступное.")
+    return False
+
+
+def get_money_for_economy(country: Country) -> bool:
+    try:
+        amount = int(input(f"Введите желаемое количество денег для перевода (всего доступно {country.army_money}."))
+        if amount > country.army_money:
+            raise InputError
+        print("Деньги переведены.")
+        country.army_money -= amount
+        country.economy_money += amount
+        return True
+    except ValueError:
+        print("Введено не число.")
+    except InputError:
+        print("Введенное количество денег превышает максимально доступное.")
+    return False
+
+
+def choose_command(actions: dict) -> int:
+    print("Доступные действия: ")
+    for n, a in actions.items(): print(f"{n}: {a}")
     while True:
         try:
             chosen_command = int(input("Введите номер команды: "))
@@ -224,37 +241,46 @@ def economy_stage(active_country: Country) -> None:
             print("Введено не число.")
         except InputError:
             print("Команды под введенном номером не существует.")
-    match int(chosen_command):
-        case 1:
-            buy_upgrades(active_country)
-        case 2:
-            print(f"Страна {active_country.country_name} пропустила ход")
-        case _:
-            pass
+    return chosen_command
+
+
+def economy_stage(active_country: Country) -> None:
+    actions = {1: "Купить улучшения", 2: "Пропустить ход", 3: "Перевести деньги на экономику с армии"}
+    is_successful = False
+    while not is_successful:
+        chosen_command = choose_command(actions)
+        match chosen_command:
+            case 1:
+                is_successful = buy_upgrades(active_country)
+            case 2:
+                is_successful = True
+                print(f"Страна {active_country.country_name} пропустила ход")
+            case 3:
+                is_successful = get_money_for_economy(active_country)
+            case _:
+                pass
 
 
 def military_stage(active_country: Country, passive_country: Country) -> None:
-    actions = {1: "Нанять солдат", 2: "Отправить солдат в отставку", 3: "Начать войну"}
-    for n, a in zip(actions.keys(), actions.values()): print(f"{n}: {a}")
-    while True:
-        try:
-            chosen_command = int(input("Введите номер команды: "))
-            if chosen_command not in actions.keys():
-                raise InputError
-            break
-        except ValueError:
-            print("Введено не число.")
-        except InputError:
-            print("Команды под введенном номером не существует.")
-    match int(chosen_command):
-        case 1:
-            recruit_soldiers(active_country)
-        case 2:
-            retire_soldiers(active_country)
-        case 3:
-            start_a_war(active_country, passive_country)
-        case _:
-            pass
+    actions = {1: "Нанять солдат", 2: "Отправить солдат в отставку", 3: "Начать войну", 4: "Перевести деньги\
+                на армию с экономики", 5: "Пропустить ход"}
+    is_successful = False
+    while not is_successful:
+        chosen_command = choose_command(actions)
+        match int(chosen_command):
+            case 1:
+                is_successful = recruit_soldiers(active_country)
+            case 2:
+                is_successful = retire_soldiers(active_country)
+            case 3:
+                is_successful = start_a_war(active_country, passive_country)
+            case 4:
+                is_successful = get_money_for_army(active_country)
+            case 5:
+                is_successful = True
+                print(f"Страна {active_country.country_name} пропустила ход")
+            case _:
+                pass
 
         
 def make_a_move(active_country: Country, passive_country: Country) -> None:
