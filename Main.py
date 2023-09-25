215ec29b5b53c10825e1f9c51b4bf892f27afa22
Olga Matveeva <matveevaolga2882@gmail.com>
2023-09-25 14:18:14 +0300

    fixed main module

--- Main.py
+++ Main.py
@@ -26,68 +26,82 @@ def country_choice(taken_n=-1) -> int:
     return n
 
 
-while True:
+def command_choice() -> int:
     for numb, comm in commands.items():
         print(numb, ': ', comm, sep='')
     while True:
         try:
-            command = int(input("Выберите номер команды: "))
-            if not len(countries) and command != 1:
+            command_num = int(input("Выберите номер команды: "))
+            if not len(countries) and command_num != 1:
                 raise InputError
             break
         except ValueError:
             print("Введено не число.")
         except InputError:
             print('Вы еще не создали ни одной страны.')
-    match command:
-        case 1:
-            country, country_name = create_country(list(countries.keys()))
-            countries[country.country_name] = country
-        case 2:
-            n = country_choice()
-            country = list(countries.values())[n]
-            print(country)
-        case 3:
-            n = country_choice()
-            country = list(countries.values())[n]
-            print(country.economy)
-        case 4:
-            n = country_choice()
-            country = list(countries.values())[n]
-            army = choose_army(country)
-            print(army)
-        case 5:
-            if len(countries) == 1:
-                print(f'Стране {list(countries.keys())[0]} не с кем воевать.')
-                continue
-            print('Выбор первой воюющей страны:')
-            n1 = country_choice()
-            country1 = countries[list(countries.keys())[n1]]
-            print('Выбор второй воюющей страны:')
-            n2 = country_choice(n1)
-            country2 = countries[list(countries.keys())[n2]]
-            for _ in range(2):
-                make_a_move(country1, country2)
-                make_a_move(country2, country1)
-        case 6:
-            n = country_choice()
-            country = list(countries.keys())[n]
-            country = countries[country]
-            recruit_soldiers(country)
-        case 7:
-            n = country_choice()
-            country = list(countries.keys())[n]
-            country = countries[country]
-            retire_soldiers(country)
-        case 8:
-            n = country_choice()
-            country = list(countries.keys())[n]
-            buy_upgrades(countries[country])
-        case 9:
-            n = country_choice()
-            country = list(countries.values())[n]
-            country.armies.append(create_army())
-        case 10:
-            break
-        case _:
-            print('Неверный номер команды.')
+    return command_num
+
+
+def manage_war() -> None:
+    if len(countries) == 1:
+        print(f'Стране {list(countries.keys())[0]} не с кем воевать.')
+        return
+    print('Выбор атакующей страны:')
+    n1 = country_choice()
+    country1 = countries[list(countries.keys())[n1]]
+    print('Выбор защищающейся страны:')
+    n2 = country_choice(n1)
+    country2 = countries[list(countries.keys())[n2]]
+    for _ in range(2):
+        make_a_move(country1, country2)
+        make_a_move(country2, country1)
+
+
+def start():
+    while True:
+        command = command_choice()
+        match command:
+            case 1:
+                country, country_name = create_country(list(countries.keys()))
+                countries[country.country_name] = country
+            case 2:
+                n = country_choice()
+                country = list(countries.values())[n]
+                print(country)
+            case 3:
+                n = country_choice()
+                country = list(countries.values())[n]
+                print(country.economy)
+            case 4:
+                n = country_choice()
+                country = list(countries.values())[n]
+                army = choose_army(country)
+                print(army)
+            case 5:
+                manage_war()
+            case 6:
+                n = country_choice()
+                country = list(countries.keys())[n]
+                country = countries[country]
+                recruit_soldiers(country)
+            case 7:
+                n = country_choice()
+                country = list(countries.keys())[n]
+                country = countries[country]
+                retire_soldiers(country)
+            case 8:
+                n = country_choice()
+                country = list(countries.keys())[n]
+                buy_upgrades(countries[country])
+            case 9:
+                n = country_choice()
+                country = list(countries.values())[n]
+                country.armies.append(create_army(country))
+            case 10:
+                break
+            case _:
+                print('Неверный номер команды.')
+
+
+if __name__ == '__main__':
+    start()
